b63a630af3e0012037297d30a6dd7ccaec6942c4
Pedro Mello <contato@cicatriz.dev>
2026-03-11 19:21:49 -0300

    codigo aula 4.3

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 import './styles/reset.css';
 
-// React 17: ReactDOM.render (não createRoot)
-// TODO: migrar para React 18 createRoot
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')!).render(<App />);
